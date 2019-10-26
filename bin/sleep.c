40c2476cd916b33b09f32a7131a407f387f9267a
nefenit <nefenit@gmail.com>
2019-10-26 15:26:50 +0200

    added sleep

---
+++ bin/sleep.c
@@ -0,0 +1,48 @@
+#define PROGRAM_NAME          "sleep"
+#define PROGRAM_VERSION       "1.0"
+#define PACKAGE_NAME          "Unitrix bin"
+#define COPYRIGTH_HOLDER      "Bartosz Mierzynski"
+#define COPYRIGTH_YEAR        "2019"
+#define LICENSE_ABBREVIATION  "none"
+#define LICENSE_LINE          ""
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <signal.h>
+
+void usage(int status) {
+	printf("usage: %s time\n", PROGRAM_NAME);
+	exit(status);
+}
+
+void handle(int sig) {
+	exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv) {
+	int c;
+	
+	setlocale(LC_ALL, "");
+	
+	signal(SIGALRM, handle);
+	
+	while ((c = getopt(argc, argv, "")) != -1)
+		switch(c) {
+		case '?':
+		default:
+			usage(EXIT_FAILURE);
+	}
+
+	argc -= optind;
+
+	if(argc != 1) {
+		usage(EXIT_FAILURE);
+	}
+
+	sleep(strtoul(argv[1], NULL, 0));
+	
+	return EXIT_SUCCESS;
+}
